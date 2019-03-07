44c76deb05cd4482064a56a032b67070115b4655
PaphobGun <paphob.a@gmail.com>
2019-03-07 22:07:46 +0700

    Updates

---
+++ precache-manifest.2c576af434fff061ab338ef5a0f635d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "5b7a321fc8acc0bb40ba",
+    "url": "/contactmanager/static/js/main.5b7a321f.chunk.js"
+  },
+  {
+    "revision": "3de7afd952f012d767ec",
+    "url": "/contactmanager/static/js/2.3de7afd9.chunk.js"
+  },
+  {
+    "revision": "5b7a321fc8acc0bb40ba",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "3de7afd952f012d767ec",
+    "url": "/contactmanager/static/css/2.a04dc651.chunk.css"
+  },
+  {
+    "revision": "4f3d5629293b350e70e044fc652d61de",
+    "url": "/contactmanager/index.html"
+  }
+];
